eeba9ec37930d2fb0029187bacd0fa6bbb60dffa
Kristina <ccthw@protonmail.com>
2020-04-16 17:06:50 +0300

    Add index.js, projects.js, update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require ('express'); 
-const { projects } = require ('./data.json'); 
 
 const app = express (); 
 
@@ -7,18 +6,11 @@ app.use ('/static', express.static ('public'));
 
 app.set ('view engine', 'pug'); 
 
-app.get ('/', (req, res) => {
-    res.render ('index', { projects });
-});
+const mainRoutes = require ('./routes/index');
+const projectsRoutes = require ('./routes/projects');
 
-app.get ('/about', (req, res) => {
-    res.render ('about');
-});
-
-app.get ('/projects/:id', (req, res) => {
-    const { id } = req.params;
-    res.render ('projects', { id });
-});
+app.use (mainRoutes);
+app.use (projectsRoutes);
 
 app.listen (3000, () => {
     console.log ('The application is running on localhost:3000');
---
+++ routes/projects.js
@@ -0,0 +1,10 @@
+const express = require ('express'); 
+const router = express.Router ();
+const { projects } = require ('../data.json'); 
+
+router.get ('/projects/:id', (req, res) => {
+    const { id } = req.params;
+    res.render ('project', { projects });
+});
+
+module.exports = router; 
